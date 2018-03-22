9137ba3f9f95cbc93a14f4ad70b118f4675424a3
soluyanow <soluyanow@gmail.com>
2018-03-22 10:57:22 +0300

    dmin.php

--- admin.php
+++ admin.php
@@ -31,18 +31,42 @@
     }
 
     function login() {
-
-
+        $results = array();
+	    $results['pageTitle'] = "Admin Login | Widget News";
+	    if (isset( $_POST['login'])) {
+	        if ($_POST['username'] == ADMIN_USERNAME && $_POST['password'] == ADMIN_PASSWORD) {
+                $_SESSION['username'] = ADMIN_USERNAME;
+                header( "Location: admin.php");
+            } else {
+                $results['errorMessage'] = "Incorrect username or password. Please try again.";
+                require(TEMPLATE_PATH."/admin/loginForm.php");
+            }
+        } else {
+             require( TEMPLATE_PATH."/admin/loginForm.php");
+        }
     }
 
     function logout() {
-
-
+        unset($_SESSION['username']);
+	    header( "Location: admin.php");
     }
 
     function newArticle() {
+        $results = array();
+	    $results['pageTitle'] = "New Article";
+	    $results['formAction'] = "newArticle";
 
-
+	    if ( isset( $_POST['saveChanges'] ) ) {
+            $article = new Article;
+            $article->storeFormValues( $_POST );
+            $article->insert();
+            header( "Location: admin.php?status=changesSaved" );
+	    } elseif ( isset( $_POST['cancel'] ) ) {
+            header( "Location: admin.php" );
+        } else {
+            $results['article'] = new Article;
+            require( TEMPLATE_PATH . "/admin/editArticle.php" );
+	    }
     }
 
     function deleteArticle() {
